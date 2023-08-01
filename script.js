cf7667b0ea741526e9722395a0b28b869e3ea47f
Taylor Moore <122215363+tmoore1987@users.noreply.github.com>
2023-08-01 16:30:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+const form = document.querySelector('form');
+const todoInput = document.querySelector('#todo-input');
+const addButton = document.querySelector('#add-button');
+const todoList = document.querySelector('#todo-list');
+
+let todos = []
+
+//add new tasl
+
+const addTodo = () => {
+    const todoText = todoInput.value.trim();
+
+    if (todoText.length > 0) {
+        const todo = {
+            id: Date.now(),
+            text: todoText,
+            completed: false
+        }
+
+        todos.push(todo);
+
+        todoInput.value = '';
+
+        renderTodos()
+    }
+
+}
+
+//toggle and mark the completion of task
+
+const toggleCompleted = (id) => {
+    todos = todos.map(todo => {
+        if (todo.id === id) {
+            todo.completed = !todo.completed
+        }
+        return todo;
+    })
+
+    renderTodos()
+   
+}
+
+//delete tasks
+
+const deleteTodo = (id) => {
+    todos = todos.filter(todo => todo.id !==id);
+
+    renderTodos();
+
+}
+
+//capture input
+
+const renderTodos = () => {
+    todoList.innerHTML = '';
+
+    todos.forEach(todo => {
+        const todoItem = document.createElement('li');
+        const todoText = document.createElement('span');
+        const todoDeleteButton = document.createElement('button');
+
+            todoText.textContent = todo.text;
+            todoDeleteButton.textContent = 'Delete';
+
+            todoDeleteButton.addEventListener('click', () => deleteTodo(todo.id))
+
+            if (todo.completed) {
+                todoItem.classList.add('completed')
+            }
+
+            todoItem.addEventListener('click', () => 
+            toggleCompleted(todo.id))
+            
+            todoItem.appendChild(todoText);
+            todoItem.appendChild(todoDeleteButton);
+            todoList.appendChild(todoItem)
+    })
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    addTodo()
+})
+
+renderTodos()
+
+
